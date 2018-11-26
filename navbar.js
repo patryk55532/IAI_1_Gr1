4ccea10adce76837580415e37c92f6a433ca68b4
patryk55532 <30683@student.pwsztar.edu.pl>
2018-11-26 17:27:29 +0100

    Kolos

---
+++ navbar.js
@@ -0,0 +1,12 @@
+let nav='<div>\n' +
+    ' <nav>\n' +
+    '     <ul>\n' +
+    '        <li><a href="kontakt.html">Strona głowna</a></li>\n' +
+    '
+    '
+     </nav>
+    '</div>';
+
+let navbar=document.querySelector('.navbar');
+
+navbar.innerHTML = nav;
